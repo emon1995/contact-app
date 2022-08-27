97cd20b6e0e1758823b94a29035e9a4297e98747
emon1995 <emonhossain995@gmail.com>
2022-08-27 20:52:00 +0600

    Alert component

--- src/navigations/SideMenu/SideMenu.js
+++ src/navigations/SideMenu/SideMenu.js
@@ -1,16 +1,46 @@
-import { useNavigation } from "@react-navigation/native";
-import { Image, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { DrawerActions, useNavigation } from "@react-navigation/native";
+import {
+    Alert,
+  Image,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Container from "../../components/common/Container";
 import { SETTINGS } from "../../constants/routeNames";
 import styles from "./styles";
 
 const SideMenu = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const menuItems = [
-        {icon: <Text>T</Text>, name: "Settings", onPress: () => navigation.navigate(SETTINGS)},
-        {icon: <Text>T</Text>, name: "Logout", onPress: () => navigation.navigate(SETTINGS)},
-    ]
+  const handleLogout = () => {
+    // () => navigation.toggleDrawer() ;
+    navigation.dispatch(DrawerActions.toggleDrawer());
+    Alert.alert("Logout!", "Are you sure you want to logout?", [
+        {
+            text: "Cancel",
+            onPress: () => {},
+        },
+        {
+            text: "OK",
+            onPress: () => {},
+        }
+    ])
+  }
+
+  const menuItems = [
+    {
+      icon: <Text>T</Text>,
+      name: "Settings",
+      onPress: () => navigation.navigate(SETTINGS),
+    },
+    {
+      icon: <Text>T</Text>,
+      name: "Logout",
+      onPress: () => handleLogout(),
+    },
+  ];
   return (
     <SafeAreaView>
       <Container>
@@ -20,9 +50,9 @@ const SideMenu = () => {
           source={require("../../../assets/images/logo.png")}
           style={styles.logoImage}
         />
-        <View style={{paddingHorizontal: 70}}>
-          {menuItems.map(({name, icon, onPress}) => (
-            <TouchableOpacity onPress={onPress}  key={name} style={styles.item}>
+        <View style={{ paddingHorizontal: 70 }}>
+          {menuItems.map(({ name, icon, onPress }) => (
+            <TouchableOpacity onPress={onPress} key={name} style={styles.item}>
               <View style={styles.icon}>{icon}</View>
               <Text style={styles.itemText}>{name}</Text>
             </TouchableOpacity>
